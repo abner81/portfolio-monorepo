ba1ed3d40c5ca2b0eea852a19e006645efc1aa6d
Abner Machado <54149914+abner81@users.noreply.github.com>
2024-11-04 03:17:28 -0300

    fix: fix to convert to camel case

--- apps/shared/util/helpers/src/string/convert-to-camel-case.ts
+++ apps/shared/util/helpers/src/string/convert-to-camel-case.ts
@@ -2,5 +2,11 @@ import { capitalize } from './capitalize';
 
 export function convertToCamelCase(str: string, _capitalize = true) {
   const result = str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+
+  if (_capitalize) {
+    console.log(result.charAt(0).toUpperCase() + result.slice(1));
+    return result.charAt(0).toUpperCase() + result.slice(1);
+  }
+
   return _capitalize ? capitalize(result) : result;
 }
