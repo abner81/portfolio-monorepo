9452da7cf724bfdd8854f69d494bcfc34881e488
Abner Machado <54149914+abner81@users.noreply.github.com>
2024-11-06 02:02:22 -0300

    feat: add Rent createdAt

--- apps/rent-car/domain/rent/src/entities/rent/rent.spec.ts
+++ apps/rent-car/domain/rent/src/entities/rent/rent.spec.ts
@@ -4,11 +4,12 @@ import { Rent, RentProps } from './rent';
 describe('Rent Entity', () => {
   const props: RentProps = {
     id: EntityId.create().value,
+    status: 'Reserved',
+    createdAt: new Date(),
   };
 
   it('should render Rent with correct values and validations', () => {
     const valid = new Rent(props);
-
     expect(valid).toBeDefined();
   });
 });
--- apps/rent-car/domain/rent/src/entities/rent/rent.ts
+++ apps/rent-car/domain/rent/src/entities/rent/rent.ts
@@ -1,10 +1,17 @@
 import { Entity } from '@monorepo/arch/domain';
-import { EntityId, EntityIdProps } from '@monorepo/value-objects';
+import {
+  DateValueObject,
+  EntityId,
+  EntityIdProps,
+} from '@monorepo/value-objects';
+import { Status, StatusProps } from './value-objects';
 
-export type RentProps = {} & EntityIdProps;
+export type RentProps = { createdAt: Date } & EntityIdProps & StatusProps;
 
 export type RentState = {
   id: EntityId;
+  status: Status;
+  createdAt: DateValueObject;
 };
 
 export class Rent extends Entity<RentProps, RentState> {
@@ -12,15 +19,27 @@ export class Rent extends Entity<RentProps, RentState> {
     return this.state.id;
   }
 
+  public get status() {
+    return this.state.status;
+  }
+
+  public get createdAt() {
+    return this.state.createdAt;
+  }
+
   protected parse(props: RentProps): RentState {
     const id = new EntityId(props);
+    const status = new Status(props);
+    const createdAt = new DateValueObject({ date: props.createdAt });
 
-    return { id };
+    return { id, status, createdAt };
   }
 
   export(): Required<RentProps> {
     return {
       id: this.id.value,
+      status: this.status.value,
+      createdAt: this.createdAt.value,
     };
   }
 }
