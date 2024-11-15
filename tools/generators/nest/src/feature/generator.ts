69b073e2b7a95afd05794b143e5d1a4e65a1ea7a
Abner Machado <54149914+abner81@users.noreply.github.com>
2024-11-15 19:06:43 -0300

    feat: generate service trigger on nest feat generator

--- tools/generators/nest/src/feature/generator.ts
+++ tools/generators/nest/src/feature/generator.ts
@@ -3,9 +3,17 @@ import { CreateNestFeatureGeneratorSchema } from './schema';
 import {
   appendContent,
   convertToCamelCase,
+  convertToKebabCase,
   removePrefixTo,
 } from '@monorepo/helpers';
 import * as path from 'path';
+import * as ts from 'typescript';
+import {
+  InsertChange,
+  applyToUpdateRecorder,
+} from '@schematics/angular/utility/change';
+
+export type FileType = 'controller' | 'service' | 'repository';
 
 class FeatureGenerator {
   public tree: Tree;
@@ -33,18 +41,30 @@ class FeatureGenerator {
       this.params?.folder ?? removePrefixTo(this.targetName.fileName);
     return names(_rawFolder).fileName;
   }
-  private get controllersPath() {
-    return `apps/${this.projectPath}/src/${this.context}/controllers`;
+  private get projectSrcPath() {
+    return `apps/${this.projectPath}/src`;
   }
-  private get targetPath() {
+  private get controllersPath() {
+    return `${this.projectSrcPath}/${this.context}/controllers`;
+  }
+  private get servicesPath() {
+    return `${this.applicationContextPath}/services`;
+  }
+  private get controllerTargetPath() {
     return `${this.controllersPath}/${this.folder}/${this.targetName.fileName}`;
   }
+  private get serviceTargetPath() {
+    return `${this.servicesPath}/${this.folder}/${this.targetName.fileName}`;
+  }
+  private get applicationContextPath() {
+    return `apps/${this.projactName}/application/src/${this.context}`;
+  }
 
-  private makeGenerateFile(fileType: string) {
+  private makeGenerateFile(fileType: string, _path: string) {
     return generateFiles(
       this.tree,
       path.join(__dirname, 'files', fileType),
-      this.targetPath,
+      _path,
       {
         folder: this.folder,
         context: this.context,
@@ -54,47 +74,127 @@ class FeatureGenerator {
         repositoryName: this.params.repositoryName,
         name: this.targetName.fileName,
         convertToCamelCase,
+        convertToKebabCase,
         convertToConstant: (name: string) =>
-          name.toUpperCase().replace('-', '_'),
+          name.toUpperCase().replaceAll('-', '_'),
       }
     );
   }
 
-  private mergeControllersIndex() {
+  private mergeIndex(path: string) {
     appendContent(
       this.tree,
-      `${this.controllersPath}/index.ts`,
+      `${path}/index.ts`,
       `export * from "./${this.folder}"`
     );
   }
 
-  private mergeTargetFolderIndex() {
+  private mergeTargetFolderIndex(type: FileType, folderPath: string) {
     const folderIndexPath = `${this.controllersPath}/${this.folder}`;
     appendContent(
       this.tree,
       `${folderIndexPath}/index.ts`,
-      `export * from "./${this.targetName.fileName}/${this.targetName.fileName}.controller"`
+      `export * from "./${this.targetName.fileName}/${this.targetName.fileName}.${type}"`
     );
   }
 
+  private addServiceInConstants() {
+    const constantName = `${this.targetName.constantName}_SERVICE`;
+    const interfaceName = `I${this.targetName.className}Service`;
+
+    appendContent(
+      this.tree,
+      `${this.projectSrcPath}/config/constants.ts`,
+      `export const ${constantName} = '${interfaceName}';`
+    );
+  }
+
+  private addControllerToModule() {
+    const modulePath = `${this.projectSrcPath}/${this.context}/${this.context}.module.ts`;
+    const controllerClassName = `${this.targetName.className}Controller`;
+    const controllerPath = `./controllers`;
+
+    if (!this.tree.exists(modulePath)) {
+      throw new Error(`Module file ${modulePath} not found`);
+    }
+
+    let moduleSource = this.tree.read(modulePath, 'utf-8')!;
+
+    // Step 1: Add the import statement for the new controller if not present
+    const controllerImportRegex = new RegExp(
+      `import\\s*{([^}]*)}\\s*from\\s*'${controllerPath}';`
+    );
+    if (controllerImportRegex.test(moduleSource)) {
+      moduleSource = moduleSource.replace(
+        controllerImportRegex,
+        (match, imports) => {
+          const updatedImports = imports
+            .split(',')
+            .map((imp) => imp.trim())
+            .filter((imp) => imp);
+          if (!updatedImports.includes(controllerClassName)) {
+            updatedImports.push(controllerClassName);
+          }
+          return `import { ${updatedImports.join(
+            ', '
+          )} } from '${controllerPath}';`;
+        }
+      );
+    } else {
+      // Add new import statement if it doesn't exist
+      moduleSource =
+        `import { ${controllerClassName} } from '${controllerPath}';\n` +
+        moduleSource;
+    }
+
+    // Step 2: Add the controller to the @Module 'controllers' array
+    if (moduleSource.includes('controllers: [')) {
+      moduleSource = moduleSource.replace(
+        /controllers: \[([^\]]*)\]/,
+        (match, controllers) =>
+          `controllers: [${controllers.trim()}, ${controllerClassName}]`
+      );
+    } else {
+      moduleSource = moduleSource.replace(
+        '@Module({',
+        `@Module({\n  controllers: [${controllerClassName}],`
+      );
+    }
+
+    // Write the updated module source back to the file
+    this.tree.write(modulePath, moduleSource);
+  }
+
   private generateController() {
-    this.makeGenerateFile('controller');
-    this.mergeControllersIndex();
-    this.mergeTargetFolderIndex();
+    this.addServiceInConstants();
+    this.makeGenerateFile('controller', this.controllerTargetPath);
+
+    this.mergeIndex(this.controllersPath);
+    this.mergeTargetFolderIndex(
+      'controller',
+      `${this.controllersPath}/${this.folder}`
+    );
+
+    this.addControllerToModule();
   }
 
   private generateService() {
-    this.makeGenerateFile('service');
+    this.makeGenerateFile('service', this.serviceTargetPath);
+    this.mergeIndex(this.servicesPath);
+    this.mergeTargetFolderIndex(
+      'service',
+      `${this.servicesPath}/${this.folder}`
+    );
   }
 
   private generateRepository() {
-    this.makeGenerateFile('repository');
+    this.makeGenerateFile('repository', '');
   }
 
   static execute(tree: Tree, params: CreateNestFeatureGeneratorSchema) {
     const instance = new FeatureGenerator(tree, params);
-    instance.generateController();
-    // instance.generateService();
+    // instance.generateController();
+    instance.generateService();
   }
 }
 
